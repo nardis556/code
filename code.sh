15f74d4ab3f129ad2b4017e8abc5d5f1a39533e5
nardis556 <82609175+nardis556@users.noreply.github.com>
2022-12-27 09:46:12 -0600

    Update code.sh

--- code.sh
+++ code.sh
@@ -2,33 +2,59 @@
 
 function deps() {
     bash -c 'sudo apt update'
-    bash -c 'sudo apt install curl git gcc make make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y'
+    bash -c 'sudo apt install curl git gcc make make build-essential \
+            libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev \
+            wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev \
+            libxmlsec1-dev libffi-dev liblzma-dev -y'
     bash -c 'sudo apt upgrade -y'
 }
 
-curl -fsSL https://code-server.dev/install.sh | sh
-systemctl enable --now code-server@$USER
+function code() {
+    bash -c 'curl -fsSL https://code-server.dev/install.sh | sh'
+    bash -c 'systemctl enable --now code-server@$USER'
+}
+
 
 function nvm() {
-    echo 'installing nvm'
-    bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash'
+    bash -c 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash'
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 }
 
 function pyenv() {
-    echo 'install pyenv'
     bash -c 'git clone https://github.com/pyenv/pyenv.git ~/.pyenv'
     echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
     echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
     echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 }
 
+echo 'Would you like to install pyenv? Enter: y/n'
+read pyenvCond
+
+echo 'Would you like to install nvm? Enter: y/n'
+read nvmCond
+
+echo 'Would you like to install code-server? Enter: y/n'
+read codeCond
+
+echo 'installing dependencies'
 deps
-nvm
-pyenv
 
-echo 'complete'
+if [ $pyenvCond == 'y' ]
+then
+  echo "installing pyenv"
+  pyenv
+fi
+if [ $nvmCond == 'y' ]
+then
+  echo "installing nvm"
+  nvm
+fi
+if [ $codeCond == 'y' ]
+then
+  echo "installing code-server"
+  code
+fi
 
-bash -c 'exec bash'
+echo "Done"
